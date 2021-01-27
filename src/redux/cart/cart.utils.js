39df2214de02ce825fb455416dbc08f655f85740
ltobar <luisadrianta1@gmail.com>
2021-01-26 19:13:13 -0500

    MOD: custom button on cart items - cart data groups items in cart reducer

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,17 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToAdd.id
+  );
+
+  if (existingCartItem) {
+    // Return new array because we need new version
+    // of state to re render properly
+    return cartItems.map((cartItem) =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
