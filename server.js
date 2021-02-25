40a02c7778321b9bef5904228131a148a045f8e8
ltobar <luisadrianta1@gmail.com>
2021-02-24 19:25:59 -0500

    FIX: import path to server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require('path');
 
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 // This is your real test secret API key.
