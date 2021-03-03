246261db4e1f2422212d27e9191129937a46f701
ltobar <luisadrianta1@gmail.com>
2021-03-03 14:59:50 -0500

    MOD: improve css for mobile support ADD: styled components jsx files to some components

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,13 @@ import { Route, Switch, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
-import "./App.css";
-
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component.jsx";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sing-in-and-sign-up.component.jsx";
 import CheckoutPage from "./pages/checkout/checkout.component.jsx";
-
 import Header from "./components/header/header.component.jsx";
+
+import { GlobalStyle } from "./global.styles";
 
 import { selectCurrentUser } from "./redux/user/user.selectror";
 import { checkUserSession } from "./redux/user/user.actions";
@@ -28,6 +27,7 @@ const App = ({ checkUserSession, currentUser }) => {
 
   return (
     <div>
+      <GlobalStyle />
       <Header />
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
